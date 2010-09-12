dea1db4e44ce6036e0095f1fc3d4e82793cf287f
Manfred Moitzi <mozman@gmx.at>
2010-09-12 11:05:43 +0200

    moved all check-routines to validator.py
    added several tests

--- svgwrite/shapes.py
+++ svgwrite/shapes.py
@@ -6,46 +6,51 @@
 # Copyright (C) 2010, Manfred Moitzi
 # License: GPLv3
 
+import parameter
 from base import BaseElement
-from parameter import check_coordinate
+from validator import check_coordinate
 from utils import points_to_string
 
 class Line(BaseElement):
     def __init__(self, start=(0, 0), end=(0, 0),
                  attribs=None, **kwargs):
         super(Line, self).__init__(attribs, **kwargs)
-        self.attribs['x1'] = check_coordinate(start[0])
-        self.attribs['y1'] = check_coordinate(start[1])
-        self.attribs['x2'] = check_coordinate(end[0])
-        self.attribs['y2'] = check_coordinate(end[1])
+        profile = parameter.proile
+        self.attribs['x1'] = check_coordinate(start[0], profile)
+        self.attribs['y1'] = check_coordinate(start[1], profile)
+        self.attribs['x2'] = check_coordinate(end[0], profile)
+        self.attribs['y2'] = check_coordinate(end[1], profile)
 
 class Rect(BaseElement):
     def __init__(self, start=(0, 0), width=1, height=1, rx=None, ry=None,
                  attribs=None, **kwargs):
         super(Rect, self).__init__(attribs, **kwargs)
-        self.attribs['x'] = check_coordinate(start[0])
-        self.attribs['y'] = check_coordinate(start[1])
-        self.attribs['width'] = check_coordinate(width)
-        self.attribs['height'] = check_coordinate(height)
-        if rx: self.attribs['rx'] = check_coordinate(rx)
-        if ry: self.attribs['ry'] = check_coordinate(ry)
+        profile = parameter.proile
+        self.attribs['x'] = check_coordinate(start[0], profile)
+        self.attribs['y'] = check_coordinate(start[1], profile)
+        self.attribs['width'] = check_coordinate(width, profile)
+        self.attribs['height'] = check_coordinate(height, profile)
+        if rx: self.attribs['rx'] = check_coordinate(rx, profile)
+        if ry: self.attribs['ry'] = check_coordinate(ry, profile)
 
 class Circle(BaseElement):
     def __init__(self, center=(0, 0), r=1,
                  attribs=None, **kwargs):
         super(Circle, self).__init__(attribs, **kwargs)
-        self.attribs['cx'] = check_coordinate(center[0])
-        self.attribs['cy'] = check_coordinate(center[1])
-        self.attribs['r'] = check_coordinate(r)
+        profile = parameter.proile
+        self.attribs['cx'] = check_coordinate(center[0], profile)
+        self.attribs['cy'] = check_coordinate(center[1], profile)
+        self.attribs['r'] = check_coordinate(r, profile)
 
 class Ellipse(BaseElement):
     def __init__(self, center=(0, 0), rx=1, ry=1,
                  attribs=None, **kwargs):
         super(Circle, self).__init__(attribs, **kwargs)
-        self.attribs['cx'] = check_coordinate(center[0])
-        self.attribs['cy'] = check_coordinate(center[1])
-        self.attribs['rx'] = check_coordinate(rx)
-        self.attribs['ry'] = check_coordinate(ry)
+        profile = parameter.proile
+        self.attribs['cx'] = check_coordinate(center[0], profile)
+        self.attribs['cy'] = check_coordinate(center[1], profile)
+        self.attribs['rx'] = check_coordinate(rx, profile)
+        self.attribs['ry'] = check_coordinate(ry, profile)
 
 class Polyline(BaseElement):
     def __init__(self, points=[], attribs=None, **kwargs):
--- svgwrite/utils.py
+++ svgwrite/utils.py
@@ -7,6 +7,7 @@
 # License: GPLv3
 
 import parameter
+from validator import check_tiny, check_coordinate
 
 def rgb(r=0, g=0, b=0, mode='RGB'):
     """Convert r, g, b values to a string.
@@ -23,11 +24,11 @@ def rgb(r=0, g=0, b=0, mode='RGB'):
         return value
 
     if mode.upper() == 'RGB':
-        return "rgb(%d,%d,%d)" % (int(r) & 255, int(g) & 255, int(b) & 255)
+        return u"rgb(%d,%d,%d)" % (int(r) & 255, int(g) & 255, int(b) & 255)
     elif mode == "%":
-        return "rgb(%.3f%%,%.3f%%,%.3f%%)" % (percent(r), percent(g), percent(b))
+        return u"rgb(%.3f%%,%.3f%%,%.3f%%)" % (percent(r), percent(g), percent(b))
     else:
-        raise ValueError("Invalide mode '%s'" % mode)
+        raise ValueError("Invalid mode '%s'" % mode)
 
 def value_to_string(value):
     if parameter.debug_mode and parameter.profile=='tiny' and isinstance(value, (int, float)):
@@ -36,43 +37,17 @@ def value_to_string(value):
 
 def points_to_string(points):
     strings = []
+    profile = parameter.profile
+    debug_mode = parameter.debug_mode
     for point in points:
         if isinstance(point, tuple):
-            check_coordinate(point[0])
-            check_coordinate(point[1])
-            point = "%s,%s" % point
-        strings.append(point)
-    return ' '.join(strings)
-
-def _split_coordinate(value):
-    """ Split value in (number, unit) if value has an unit or (number, None). """
-    try:
-        return float(value), None
-    except ValueError:
-        value = value.strip().lower()
-        if value[-1] == '%':
-            return float(value[:-1]), '%'
-        elif value[-2:] in parameter.valid_units:
-            return float(value[:-2]), value[-2:]
+            if len(point) != 2:
+                raise ValueError("<2-tuple> is required: '%s'" % str(point))
+            if debug_mode:
+                check_coordinate(point[0], profile)
+                check_coordinate(point[1], profile)
+            point = u"%s,%s" % point
         else:
-            raise ValueError("'%s' has not a valid svg unit." % value)
-
-def get_coordinate(value):
-    """ Get coordinate (number, unit), unit is None if value doesn't have a unit,
-    raises ValueError if not valid."""
-    number, unit = _split_coordinate(value)
-    if parameter.profile == 'tiny': # check value range of tiny profile
-        number = round(number, 4) # round to four places for tiny profile
-        check_tiny(number)
-    return number, unit
-
-def check_tiny(number):
-    """ Check if number is a valid 'tiny' coordinate, raises ValueError if not valid. """
-    if not (-32767.9999 <= number <= 32767.9999):
-        raise ValueError("'%s' out of range for baseProfile 'tiny'" % number)
-
-def check_coordinate(value):
-    """ Check if value is a valid coordinate, raises ValueError if not valid. """
-    if debug_mode:
-        number, unit = get_coordinate(value)
-    return value
+            TypeError("'%s' <string> is given, but <2-tuple> is required." % point)
+        strings.append(point)
+    return u' '.join(strings)
