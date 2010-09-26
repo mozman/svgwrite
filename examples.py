289c9d15b864f463f794546a0159d1aa47e277fe
mozman <mozman@gmx.at>
2010-09-26 09:44:26 +0200

    added example mandelbrot()

--- examples.py
+++ examples.py
@@ -11,18 +11,17 @@ import math
 import svgwrite as svg
 from svgwrite import cm, mm, rgb, deg
 
-# svg.init: define debug_mode and svg profile - optional
-# baseProfile -- full, basic or tiny baseProfile (default: full)
-# debug -- verify property-names and property-values and for each element verify
-#          valid subelements(default: False)
-svg.parameter.set_debug()
+# set_profile -- full, basic or tiny baseProfile (default: full)
+# set_debug -- verify property-names and property-values and for each element
+#     verify valid subelements(default: False)
+svg.parameter.set_debug(True)
 svg.parameter.set_profile('full')
 
-def example_empty_drawing(name):
+def empty_drawing(name):
     drawing = svg.Drawing(filename=name)
     drawing.save()
 
-def example_base_shapes_drawing(name):
+def base_shapes_drawing(name):
     drawing = svg.Drawing(filename=name)
     hlines = drawing.group(id='hlines', stroke='green')
     for y in range(20):
@@ -36,7 +35,7 @@ def example_base_shapes_drawing(name):
     shapes.add(svg.Ellipse(center=(10*cm, 15*cm), r=('5cm', '10mm')))
     drawing.save()
 
-def example_use_drawing(name):
+def use_drawing(name):
     w, h = '100%', '100%'
     dwg = svg.Drawing(filename=name, size=(w, h))
     dwg.add(svg.Rect(insert=(0,0), size=(w, h), fill='lightgray', stroke='black'))
@@ -55,7 +54,7 @@ def example_use_drawing(name):
             dwg.add(u)
     dwg.save()
 
-def example_koch_snowflake(name):
+def koch_snowflake(name):
     # Koch Snowflake and Sierpinski Triangle combination fractal using recursion
     # ActiveState Recipe 577156
     # Created by FB36 on Sat, 27 Mar 2010 (MIT)
@@ -78,7 +77,7 @@ def example_koch_snowflake(name):
         points = [(x3, y3), (x4, y4), (x5, y5)]
 
         # append new polygon to snowflake element
-        sflake.add(svg.Polygon(points))
+        snowflake.add(svg.Polygon(points))
         tf(x0, y0, x3, y3, x5, y5)
         tf(x3, y3, x1, y1, x4, y4)
         tf(x5, y5, x4, y4, x2, y2)
@@ -120,10 +119,13 @@ def example_koch_snowflake(name):
     # attention: 'stroke-width' is not a valid Python identifier, so use 'stroke_witdth'
     #   underlines '_' will be converted to dashes '-', this is true for all svg-keyword-attributs
     # if no 'id' is given ( like svg.Group(id="sflake") ), an automatic generated 'id' will be used
-    sflake = svg.Group(stroke="#000", fill="rgb(90%,90%,100%)", stroke_width=0.25)
+    snowflake = svg.Group(stroke="blue", fill="rgb(90%,90%,100%)", stroke_width=0.25)
+
+    # don't use snowflake = dwg.group(...) this automatic adds the snowflake
+    # group to 'dwg.elements' list and not to 'dwg.defs' list!
 
     # add the <g /> element to the <defs /> element of the drawing
-    dwg.defs.add(sflake)
+    dwg.defs.add(snowflake)
 
     mx2 = imgx / 2
     my2 = imgy / 2
@@ -141,23 +143,79 @@ def example_koch_snowflake(name):
     tf(x0, y0, x1, y1, x2, y2)
 
     # create an <use /> element
-    use_sflake = svg.Use(sflake)
+    use_snowflake = svg.Use(snowflake)
 
     # you can transform each <use /> element
-    use_sflake.rotate(15, center=(imgx/2, imgy/2))
+    # use_snowflake.rotate(15, center=(imgx/2, imgy/2))
 
     # insert snowflake by the <use /> element
-    dwg.add(use_sflake)
+    dwg.add(use_snowflake)
 
     # and save the drawing
     dwg.save()
 
+def mandelbrot(name):
+    ## {{{ http://code.activestate.com/recipes/577111/ (r2)
+    # Mandelbrot fractal
+    # FB - 201003254
+
+    def putpixel(pos, color):
+        mandelbrot_group.add(svg.Rect(insert=pos, size=(1, 1), fill=color))
+
+    # image size
+    imgx = 320
+    imgy = 200
+
+    # drawing defines the output size
+    dwg = svg.Drawing(name, ('32cm', '20cm'))
+
+    # define a user coordinate system with viewbox()
+    dwg.viewbox(0, 0, imgx, imgy)
+
+    mandelbrot_group = dwg.group(stroke_width=0, stroke='none')
+
+    # drawing area
+    xa = -2.0
+    xb = 1.0
+    ya = -1.5
+    yb = 1.5
+    maxIt = 255 # max iterations allowed
+
+    for y in range(imgy):
+        zy = y * (yb - ya) / (imgy - 1)  + ya
+        for x in range(imgx):
+            zx = x * (xb - xa) / (imgx - 1)  + xa
+            z = zx + zy * 1j
+            c = z
+            for i in range(maxIt):
+                if abs(z) > 2.0: break
+                z = z * z + c
+            putpixel((x, y), rgb(i % 4 * 64, i % 8 * 32, i % 16 * 16))
+    dwg.save()
+    ## end of http://code.activestate.com/recipes/577111/ }}}
 
 def main():
-    example_empty_drawing('example_empty_drawing.svg')
-    example_base_shapes_drawing('example_base_shapes_drawing.svg')
-    example_use_drawing('example_use_drawing.svg')
-    example_koch_snowflake('example_koch_snowflake.svg')
+    longrun = True
+    # short time running
+    print("start short time running examples!\n")
+
+    print("start: example_empty_drawing.svg\n")
+    empty_drawing('example_empty_drawing.svg')
+
+    print("start: example_base_shapes_drawing.svg\n")
+    base_shapes_drawing('example_base_shapes_drawing.svg')
+
+    print("start: example_use_drawing.svg\n")
+    use_drawing('example_use_drawing.svg')
+
+    print("start: example_koch_snowflake.svg\n")
+    koch_snowflake('example_koch_snowflake.svg')
+
+    if longrun:
+        print("start long time running examples!\n")
+
+        print("start: example_mandelbrot.svg\n")
+        mandelbrot('example_mandelbrot.svg')
 
 if __name__ == '__main__':
     main()
