0c19a96aa7de362db6601a3d1b0cca4f806e5b44
Manfred Moitzi <mozman@gmx.at>
2010-10-03 17:14:26 +0200

    still working on validators

--- svgwrite/data/full11typechecker.py
+++ svgwrite/data/full11typechecker.py
@@ -1,137 +1,142 @@
-def is_angle(value):
-    #angle ::= number (~"deg" | ~"grad" | ~"rad")?
-    return True
+from svgwrite import pattern
 
-def is_anything(value):
-    #anything ::= Char*
-    return True
+INVALID_NAME_CHARS = frozenset([' ', '\t', '\r', '\n', ',', '(', ')'])
+WHITESPACE = frozenset([' ', '\t', '\r', '\n'])
 
-def is_color(value):
-    #color    ::= "#" hexdigit hexdigit hexdigit (hexdigit hexdigit hexdigit)?
-    #             | "rgb(" wsp* integer comma integer comma integer wsp* ")"
-    #             | "rgb(" wsp* integer "%" comma integer "%" comma integer "%" wsp* ")"
-    #             | color-keyword
-    #hexdigit ::= [0-9A-Fa-f]
-    #comma    ::= wsp* "," wsp*
-    return True
+class Full11TypeChecker(object):
+    @staticmethod
+    def is_angle(value):
+        #angle ::= number (~"deg" | ~"grad" | ~"rad")?
+        return True
 
-def is_coordinate(value):
-    #coordinate ::= length
-    return is_length(value)
+    @staticmethod
+    def is_anything(value):
+        #anything ::= Char*
+        return True
 
-def is_frequency(value):
-    #frequency ::= number (~"Hz" | ~"kHz")
-    return True
+    @staticmethod
+    def is_color(value):
+        #color    ::= "#" hexdigit hexdigit hexdigit (hexdigit hexdigit hexdigit)?
+        #             | "rgb(" wsp* integer comma integer comma integer wsp* ")"
+        #             | "rgb(" wsp* integer "%" comma integer "%" comma integer "%" wsp* ")"
+        #             | color-keyword
+        #hexdigit ::= [0-9A-Fa-f]
+        #comma    ::= wsp* "," wsp*
+        return True
 
-def is_FuncIRI(value):
-    #FuncIRI ::= "url(" <IRI> ")"
-    return True
+    @staticmethod
+    def is_frequency(value):
+        #frequency ::= number (~"Hz" | ~"kHz")
+        return True
 
-def is_icccolor(value):
-    #icccolor ::= "icc-color(" name (comma-wsp number)+ ")"
-    return True
+    @staticmethod
+    def is_FuncIRI(value):
+        #FuncIRI ::= "url(" <IRI> ")"
+        return True
+    @staticmethod
+    def is_icccolor(value):
+        #icccolor ::= "icc-color(" name (comma-wsp number)+ ")"
+        return True
+    @staticmethod
+    def is_integer(value):
+        #integer ::= [+-]? [0-9]+
+        return True
 
-def is_integer(value):
-    #integer ::= [+-]? [0-9]+
-    return True
+    @staticmethod
+    def is_IRI(value):
+        #Internationalized Resource Identifiers
+        #a more generalized complement to Uniform Resource Identifiers (URIs)
+        return True
 
-def is_IRI(value):
-    #Internationalized Resource Identifiers
-    #a more generalized complement to Uniform Resource Identifiers (URIs)
-    return True
+    @staticmethod
+    def is_length(value):
+        #length ::= number ("em" | "ex" | "px" | "in" | "cm" | "mm" | "pt" | "pc" | "%")?
+        return True
+    is_coordinate = is_length
 
-def is_length(value):
-    #length ::= number ("em" | "ex" | "px" | "in" | "cm" | "mm" | "pt" | "pc" | "%")?
-    return True
+    @staticmethod
+    def is_list_of_T(value, t='string'):
+        def split(value):
+            #TODO: improve split function!!!!
+            return value.split()
+        #list-of-Ts ::= T
+        #               | T comma-wsp list-of-Ts
+        #comma-wsp  ::= (wsp+ ","? wsp*) | ("," wsp*)
+        #wsp        ::= (#x20 | #x9 | #xD | #xA)
+        checker = checkfuncs[t]
+        for v in split(value):
+            if not checker(v):
+                return False
+        return True
 
-def is_list_of_T(value, t='string'):
-    def split(value):
-        #TODO: improve split function!!!!
-        return value.split()
-    #list-of-Ts ::= T
-    #               | T comma-wsp list-of-Ts
-    #comma-wsp  ::= (wsp+ ","? wsp*) | ("," wsp*)
-    #wsp        ::= (#x20 | #x9 | #xD | #xA)
-    checker = checkfuncs[t]
-    for v in split(value):
-        if not checker(v):
+    @staticmethod
+    def is_semicolon_list(value):
+        #a semicolon-separated list of values
+        #               | value comma-wsp list-of-values
+        #comma-wsp  ::= (wsp+ ";" wsp*) | ("," wsp*)
+        #wsp        ::= (#x20 | #x9 | #xD | #xA)
+        return True
+
+    @staticmethod
+    def is_name(value):
+        #name  ::= [^,()#x20#x9#xD#xA] /* any char except ",", "(", ")" or wsp */
+        if not isinstance(value, basestring):
             return False
-    return True
+        name = frozenset(value)
+        if name.isdisjoint(INVALID_NAME_CHARS):
+            return True
+        return False
 
-def is_semicolon_list(value):
-    #a semicolon-separated list of values
-    #               | value comma-wsp list-of-values
-    #comma-wsp  ::= (wsp+ ";" wsp*) | ("," wsp*)
-    #wsp        ::= (#x20 | #x9 | #xD | #xA)
-    return True
+    @staticmethod
+    def is_number(value):
+        try:
+            number = float(value)
+            return True
+        except ValueError:
+            return False
 
-def is_name(value):
-    #name  ::= [^,()#x20#x9#xD#xA] /* any char except ",", "(", ")" or wsp */
-    return True
+    @staticmethod
+    def is_number_optional_number(value):
+        #number-optional-number ::= number
+        #                           | number comma-wsp number
+        return True
 
-def is_number(value):
-    #number ::= integer ([Ee] integer)?
-    #           | [+-]? [0-9]* "." [0-9]+ ([Ee] integer)?
-    return True
+    @staticmethod
+    def is_paint(value):
+        #paint ::=	"none" |
+        #           "currentColor" |
+        #           <color> [<icccolor>] |
+        #           <funciri> [ "none" | "currentColor" | <color> [<icccolor>] |
+        #           "inherit"
+        return True
 
-def is_number_optional_number(value):
-    #number-optional-number ::= number
-    #                           | number comma-wsp number
-    return True
+    @staticmethod
+    def is_percentage(value):
+        #percentage ::= number "%"
+        return True
 
-def is_paint(value):
-    #paint ::=	"none" |
-    #           "currentColor" |
-    #           <color> [<icccolor>] |
-    #           <funciri> [ "none" | "currentColor" | <color> [<icccolor>] |
-    #           "inherit"
-    return True
+    @staticmethod
+    def is_time(value):
+        #time ::= <number> (~"ms" | ~"s")?
+        return True
 
-def is_percentage(value):
-    #percentage ::= number "%"
-    return True
+    @staticmethod
+    def is_transform_list(value):
+        return True
 
-def is_time(value):
-    #time ::= <number> (~"ms" | ~"s")
-    return True
+    @staticmethod
+    def is_XML_Name(value):
+        return True
 
-def is_transform_list(value):
-    return True
+    is_string = is_anything
+    @staticmethod
+    def is_shape(value):
+        #shape ::= (<top> <right> <bottom> <left>)
+        # where <top>, <bottom> <right>, and <left> specify offsets from the
+        # respective sides of the box.
+        # <top>, <right>, <bottom>, and <left> are <length> values
+        # i.e. 'rect(5px, 10px, 10px, 5px)'
+        return True
 
-def is_XML_Name(value):
-    return True
-
-is_string = is_anything
-
-def is_shape(value):
-    #shape ::= (<top> <right> <bottom> <left>)
-    # where <top>, <bottom> <right>, and <left> specify offsets from the
-    # respective sides of the box.
-    # <top>, <right>, <bottom>, and <left> are <length> values
-    # i.e. 'rect(5px, 10px, 10px, 5px)'
-    return True
-
-checkfuncs = {
-    'angle': is_angle,
-    'anything': is_anything,
-    'color': is_color,
-    'coordinate': is_coordinate,
-    'frequency': is_frequency,
-    'FuncIRI': is_FuncIRI,
-    'icccolor': is_icccolor,
-    'interger': is_interger,
-    'IRI': is_IRI,
-    'length': is_length,
-    'list-of-T': is_list_of_T,
-    'semicolon-list': is_semicolon_list,
-    'name': is_name,
-    'number': is_number,
-    'number-optional-number': is_number_optional_number,
-    'paint': is_paint,
-    'percentage': is_percentage,
-    'time': is_time,
-    'transform-list': is_transform_list,
-    'XML-Name': is_XML_Name,
-    'string': is_string,
-    'shape': is_shape,
-}
+    def get_func_by_name(self, funcname):
+        return getattr(self, funcname, self.is_anything)
